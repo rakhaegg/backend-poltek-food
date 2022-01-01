f0826424c0134712b3d36b8cbd960657e8df0011
rakhaegg <rakhae.g@gmail.com>
2022-01-01 19:59:48 +0700

    Fix Some Bug

--- src/services/seller/ShopsService.js
+++ src/services/seller/ShopsService.js
@@ -94,10 +94,9 @@ class ShopsService {
         if (!result.rows.length) {
             throw new NotFoundError('Shop tidak ditemukan');
         }
-        const pathImage = result.rows.image
-            
-        const imageAsBase64 = fs.readFileSync(pathImage + '.jpg', 'base64');
-        return result.rows.map(shop => ({ id: shop.id, name: shop.name, no_phone: shop.no_phone, address : shop.address , image: imageAsBase64 }))
+    
+
+        return result.rows.map(shop => ({ id: shop.id, name: shop.name, no_phone: shop.no_phone, address : shop.address , image: fs.readFileSync(shop.image + '.jpg', 'base64') }))
     }
 
     async editShosById(id,credentialId,  { name, address, no_phone, image }) {
